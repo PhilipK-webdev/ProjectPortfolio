37018f93d1270a438b3f6f9192b13fd42e58c0e5
PhilipK-webdev <philipk179@gmail.com>
2020-10-03 09:48:16 -0700

    Updates

---
+++ precache-manifest.8c03e91362037a7f65bfb10958ccbad5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbecc3148740b9c1dc4ff7801738db6",
+    "url": "/ProjectPortfolio/index.html"
+  },
+  {
+    "revision": "fbe4189cc97dc31a44f1",
+    "url": "/ProjectPortfolio/static/css/main.eb2f968c.chunk.css"
+  },
+  {
+    "revision": "7892d652b8d828ed00af",
+    "url": "/ProjectPortfolio/static/js/2.9e0a8b3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ProjectPortfolio/static/js/2.9e0a8b3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe4189cc97dc31a44f1",
+    "url": "/ProjectPortfolio/static/js/main.16121209.chunk.js"
+  },
+  {
+    "revision": "ccba8ba6748c428ebd8c",
+    "url": "/ProjectPortfolio/static/js/runtime-main.28509850.js"
+  },
+  {
+    "revision": "0eb65ac84ecabcca502c514a2ef07dda",
+    "url": "/ProjectPortfolio/static/media/HEALT.0eb65ac8.png"
+  },
+  {
+    "revision": "4f6a66be187e89844ac66a73bd51b88e",
+    "url": "/ProjectPortfolio/static/media/contact.4f6a66be.svg"
+  },
+  {
+    "revision": "d87ca8ca98c64dcde0ac6c9aaa5abe14",
+    "url": "/ProjectPortfolio/static/media/developer.d87ca8ca.svg"
+  },
+  {
+    "revision": "e724a1f611e2d4717cea8806271455b6",
+    "url": "/ProjectPortfolio/static/media/groupProject.e724a1f6.png"
+  },
+  {
+    "revision": "f02052dd421b62f968854184f48c4687",
+    "url": "/ProjectPortfolio/static/media/me2.f02052dd.svg"
+  },
+  {
+    "revision": "6cf72474e7d4870819d29c42daec1535",
+    "url": "/ProjectPortfolio/static/media/myAvatar.6cf72474.png"
+  },
+  {
+    "revision": "aaa98fc084c31c9802e8898780c00c35",
+    "url": "/ProjectPortfolio/static/media/paper.aaa98fc0.svg"
+  },
+  {
+    "revision": "f954c9c8f1ad09a70ce2c6b07cfc3e08",
+    "url": "/ProjectPortfolio/static/media/project.f954c9c8.svg"
+  },
+  {
+    "revision": "68b149377e9a902b127ffffdaa70361a",
+    "url": "/ProjectPortfolio/static/media/weather.68b14937.png"
+  },
+  {
+    "revision": "e91a44d09110bfa27a8eb74a23279cdc",
+    "url": "/ProjectPortfolio/static/media/work-day.e91a44d0.png"
+  }
+]);
