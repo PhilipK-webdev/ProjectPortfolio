2de38ec2159783ed8102732f1f9bd15a077642d9
PhilipK-webdev <philipk179@gmail.com>
2020-10-08 09:14:46 -0700

    add social media account & readme

---
+++ README.md
@@ -0,0 +1,19 @@
+
+  ![followers](https://img.shields.io/github/followers/PhilipK-webdev?logoColor=red&style=social)
+  
+# Introduction: Portfolio
+
+Recently graduaded from UC-Berkley as a full stack web developer.
+ 
+
+## Built With
+
+- React
+- JavaScript
+- Node.js - express 
+
+## Authors
+
+- Philip Kouchner
+- Github link -  
+- Heroku link - 
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,7 +5,10 @@ const Footer = () => {
     return (
         <footer className="d-flex flex-column footer">
             <h2 > Made By Philip Kouchner</h2 >
-            <p><img src={developer} style={{ width: "50px" }} alt="developer" /> &copy; 2020 FullStack Web Developer, Inc.</p>
+            <p><img src={developer} style={{ width: "50px" }} alt="developer" /> &copy; 2020 FullStack Web Developer, Inc.<br />
+                <a href="https://www.linkedin.com/in/philip-kouchner-886264100/" className="text-white mr-2">Linkedin</a><a href="https://github.com/PhilipK-webdev" className="text-white">Github</a>
+            </p>
+
         </footer >
     )
 }
