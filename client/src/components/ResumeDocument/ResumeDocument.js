94bf7c15ef84731d42b85599cc6a328acbe587e8
PhilipK-webdev <philipk179@gmail.com>
2020-12-09 07:41:31 -0800

    nothing to add

--- client/src/components/ResumeDocument/ResumeDocument.js
+++ client/src/components/ResumeDocument/ResumeDocument.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import resume from "../Resume/resume2.pdf"
+import resumeNew from "../Resume/resume2.pdf"
 const ResumeDocument = () => {
     return (
         <div>
-            <a href={resume} title="Resume" target="_blank" rel="noopener noreferrer">
+            <a href={resumeNew} title="Resume" target="_blank" rel="noopener noreferrer">
                 <i className="fas fa-file-pdf" style={{ color: "rgb(255, 255, 255)", margin: "10px", fontSize: "27px", cursor: "pointer" }} >
                 </i>
             </a>
