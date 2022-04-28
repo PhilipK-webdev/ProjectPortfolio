f9858d34822b80e0633aea24601477dc0d3f91f7
Philip K <philipk179@gmail.com>
2022-04-28 16:19:52 +0300

    edit:chanign the layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/Cards/Card';
 import Cards from './components/Cards/Cards';
@@ -7,79 +7,98 @@ import Footer from './components/Footer/Footer';
 import Nav from './components/Navbar/Nav';
 import Skill from './components/Skills/Skill';
 import ScrollAnimation from 'react-animate-on-scroll';
-
+import logo from "./components/Cards/img/logo.gif";
 function App() {
-
+  const [showLogo, setShowLogo] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(true);
+      const img = (
+        <img src={logo} alt='My brand' className='img--main' />
+      );
+      setShowLogo(img);
+    })
+    setTimeout(() => {
+      setShowLogo('');
+      setIsLoading(false);
+    }, 2300)
+  }, [])
   return (
+    <div className='container__main'>
+      {isLoading ? <div className='container__main--gif'>
+        {showLogo && showLogo}
+      </div> : <Nav />}
 
-    <div style={{ position: "relative", zIndex: "0" }} className="m-0 p-0">
-      <div style={{ position: " relative", minHeight: "620px", marginBottom: "0px" }}>
-        <div style={{ position: "relative", top: "0px", minHeight: "545px", width: "100%", zIndex: "1" }}>
-          <Nav />
-          <div minheight="620" style={{ height: "100%", width: "100%", position: "absolute", top: "0px", left: "0px", zIndex: "-1", backgroundColor: "#000000" }}>
-            <div style={{ height: "100%" }}>
-            </div>
-            <svg preserveAspectRatio="none" viewBox="0 0 100 102" version="1.1" xmlns="http://www.w3.org/2000/svg"
-              style={{ width: "100%", height: "75px", position: "relative", top: "-1px" }}><path d="M0 0 L50 100 L100 0 Z" fill="#000000"></path></svg>
-          </div>
-          <div className="container-fluid p-0">
-            <div className="text-center col" id="intro">
-              <div className="continer-fluid">
-                <ScrollAnimation animateIn="slideInDown">
-                  <div id="name">
-                    Kouchner Philip
-                  </div>
+    </div>
+    // <div style={{ position: "relative", zIndex: "0" }} className="m-0 p-0">
+    //   <div style={{ position: " relative", minHeight: "620px", marginBottom: "0px" }}>
+    //     <div style={{ position: "relative", top: "0px", minHeight: "545px", width: "100%", zIndex: "1" }}>
+    //       <Nav />
+    //       <div minheight="620" style={{ height: "100%", width: "100%", position: "absolute", top: "0px", left: "0px", zIndex: "-1", backgroundColor: "#000000" }}>
+    //         <div style={{ height: "100%" }}>
+    //         </div>
+    //         <svg preserveAspectRatio="none" viewBox="0 0 100 102" version="1.1" xmlns="http://www.w3.org/2000/svg"
+    //           style={{ width: "100%", height: "75px", position: "relative", top: "-1px" }}><path d="M0 0 L50 100 L100 0 Z" fill="#000000"></path></svg>
+    //       </div>
+    //       <div className="container-fluid p-0">
+    //         <div className="text-center col" id="intro">
+    //           <div className="continer-fluid">
+    //             <ScrollAnimation animateIn="slideInDown">
+    //               <div id="name">
+    //                 Kouchner Philip
+    //               </div>
 
-                  <div className="pb-5 mb-2 col" id="webDev">
-                    Software Engineer - Full Stack
-                  </div>
-                </ScrollAnimation>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    //               <div className="pb-5 mb-2 col" id="webDev">
+    //                 Software Engineer - Full Stack
+    //               </div>
+    //             </ScrollAnimation>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
 
-      <div className="row m-0">
-        <div style={{ position: "relative", minHeight: "300px", marginBottom: "0px" }}>
-          <div style={{ height: "100px" }}></div>
-          <div style={{ position: "relative", top: "0px", minHeight: "150px", width: "100%", zIndex: "1" }}>
-            <Card />
-          </div>
-          <div minheight="300" style={{ height: "100%", width: "100%", position: "absolute", top: "0px", left: " 0px", zIndex: "-2", backgroundColor: "rgb(230, 219, 201)" }}><div style={{ height: "100%" }}></div>
-            <svg preserveAspectRatio="none" viewBox="0 0 100 102" version="1.1" xmlns="http://www.w3.org/2000/svg"
-              style={{ width: "100%", height: "75px", position: "relative", top: "-1px" }}><path d="M0 0 L50 100 L100 0 Z" fill="#e6dbc9"></path></svg></div>
-        </div>
-        <div style={{ position: "relative", minHeight: "675px", marginBottom: "0px" }} >
-          <div style={{ height: "100px" }}></div>
-          <div style={{ position: "relative", top: "0px", minHeight: "500px", width: "100%", zIndex: "1" }}>
-            <Cards />
-          </div>
-          <div minheight="675" style={{ height: "100%", width: "100%", position: "absolute", top: "0px", left: " 0px", zIndex: "-3", backgroundColor: "#000000" }}>
-            <div style={{ height: "100%" }}></div>
-            <svg preserveAspectRatio="none" viewBox="0 0 100 102" version="1.1" xmlns="http://www.w3.org/2000/svg"
-              style={{ width: "100%", height: "75px", position: "relative", top: "-1px" }}><path d="M0 0 L50 100 L100 0 Z" fill="#000000"></path></svg></div>
-        </div>
-        <div style={{ position: "relative", minHeight: "675px", marginBottom: "0px" }} >
-          <div style={{ height: "100px" }}></div>
-          <div style={{ position: "relative", top: "0px", minHeight: "500px", width: "100%", zIndex: "1" }}>
-            <Skill />
-          </div>
-          <div minheight="675" style={{ height: "100%", width: "100%", position: "absolute", top: "0px", left: " 0px", zIndex: "-4", backgroundColor: "rgb(230, 219, 201)" }}>
-            <div style={{ height: "100%" }}>
-            </div>
-            <svg preserveAspectRatio="none" viewBox="0 0 100 102" version="1.1" xmlns="http://www.w3.org/2000/svg"
-              style={{ width: "100%", height: "75px", position: "relative", top: "-1px" }}><path d="M0 0 L50 100 L100 0 Z" fill="#e6dbc9"></path></svg></div>
-        </div>
+    //   <div className="row m-0">
+    //     <div style={{ position: "relative", minHeight: "300px", marginBottom: "0px" }}>
+    //       <div style={{ height: "100px" }}></div>
+    //       <div style={{ position: "relative", top: "0px", minHeight: "150px", width: "100%", zIndex: "1" }}>
+    //         <Card />
+    //       </div>
+    //       <div minheight="300" style={{ height: "100%", width: "100%", position: "absolute", top: "0px", left: " 0px", zIndex: "-2", backgroundColor: "rgb(230, 219, 201)" }}><div style={{ height: "100%" }}></div>
+    //         <svg preserveAspectRatio="none" viewBox="0 0 100 102" version="1.1" xmlns="http://www.w3.org/2000/svg"
+    //           style={{ width: "100%", height: "75px", position: "relative", top: "-1px" }}><path d="M0 0 L50 100 L100 0 Z" fill="#e6dbc9"></path></svg></div>
+    //     </div>
+    //     <div style={{ position: "relative", minHeight: "675px", marginBottom: "0px" }} >
+    //       <div style={{ height: "100px" }}></div>
+    //       <div style={{ position: "relative", top: "0px", minHeight: "500px", width: "100%", zIndex: "1" }}>
+    //         <Cards />
+    //       </div>
+    //       <div minheight="675" style={{ height: "100%", width: "100%", position: "absolute", top: "0px", left: " 0px", zIndex: "-3", backgroundColor: "#000000" }}>
+    //         <div style={{ height: "100%" }}></div>
+    //         <svg preserveAspectRatio="none" viewBox="0 0 100 102" version="1.1" xmlns="http://www.w3.org/2000/svg"
+    //           style={{ width: "100%", height: "75px", position: "relative", top: "-1px" }}><path d="M0 0 L50 100 L100 0 Z" fill="#000000"></path></svg></div>
+    //     </div>
+    //     <div style={{ position: "relative", minHeight: "675px", marginBottom: "0px" }} >
+    //       <div style={{ height: "100px" }}></div>
+    //       <div style={{ position: "relative", top: "0px", minHeight: "500px", width: "100%", zIndex: "1" }}>
+    //         <Skill />
+    //       </div>
+    //       <div minheight="675" style={{ height: "100%", width: "100%", position: "absolute", top: "0px", left: " 0px", zIndex: "-4", backgroundColor: "rgb(230, 219, 201)" }}>
+    //         <div style={{ height: "100%" }}>
+    //         </div>
+    //         <svg preserveAspectRatio="none" viewBox="0 0 100 102" version="1.1" xmlns="http://www.w3.org/2000/svg"
+    //           style={{ width: "100%", height: "75px", position: "relative", top: "-1px" }}><path d="M0 0 L50 100 L100 0 Z" fill="#e6dbc9"></path></svg></div>
+    //     </div>
 
-      </div>
+    //   </div>
 
-      <div style={{ zIndex: "-5", paddingTop: "75px", position: "relative", backgroundColor: "#000000" }}>
-        <footer data-test="footer" className="page-footer font-small ">
-          <Contact />
-        </footer>
-      </div>
-    </div >
+    //   <div style={{ zIndex: "-5", paddingTop: "75px", position: "relative", backgroundColor: "#000000" }}>
+    //     <footer data-test="footer" className="page-footer font-small ">
+    //       <Contact />
+    //     </footer>
+    //   </div>
+    // </div >
 
   );
 }
--- client/src/components/Navbar/Nav.js
+++ client/src/components/Navbar/Nav.js
@@ -4,27 +4,60 @@ const Nav = () => {
     return (
         <div className="row m-0">
             <div className="container-fluid p-0" id="container">
-                <nav className="navbar navbar-expand-lg navbar-dark">
-                    <a href="#about" style={{ marginRight: "10px", textDecoration: "none" }} id="myName">Home</a>
-                    <button className="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#basic-navbar-nav"
-                        aria-controls="basic-navbar-nav" aria-expanded="false" aria-label="Toggle navigation"><span className="dark-blue-text"><i
+                <nav className="navbar navbar-expand-lg">
+                    {/* <a href="#about"
+                        className="ml-auto" id="myName"><span className='p-tag'>P</span></a> */}
+                    <button className="navbar-toggler toggler-example" type="button" data-toggle="collapse"
+                        data-target="#basic-navbar-nav"
+                        aria-controls="basic-navbar-nav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="dark-blue-text"><i
                             className="fas fa-bars fa-1x"></i></span></button>
-                    <div className="navbar-collapse collapse" id="basic-navbar-nav" >
-                        <ul className="mr-auto navbar-nav" >
-                            <li className="nav-item  ">
-                                <a className="nav-link text-primary" href="#about" >About Me</a>
+                    <div className="navbar-collapse collapse" id="basic-navbar-nav" style={{ marginTop: '3vh' }}>
+                        <ul className="navbar-nav ml-auto" style={{ position: "relative" }}>
+                            <li className="nav-item " >
+                                <a className="nav-link " href="#about" style={{
+                                    position: "absolute", top: '0',
+                                    right: '44vw', width: '10vw', color: 'rgba(255, 255, 255, 1)',
+                                    fontWeight: '400', fontStyle: 'normal', fontSize: '1.5vw'
+                                }}><span >01.</span>&nbsp;About</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-primary" href="#project">Projects</a>
+                                <a className="nav-link" href="#project" style={{
+                                    position: "absolute", top: '0',
+                                    right: '36vw', width: '10vw', color: 'rgba(255, 255, 255, 1)',
+                                    fontWeight: '400', fontStyle: 'normal', fontSize: '1.5vw'
+                                }}><span >02.</span>&nbsp;Experience</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-primary link" href="#skill">Skills</a>
+                                <a className="nav-link" href="#skill" style={{
+                                    position: "absolute", top: '0',
+                                    right: '26vw', width: '10vw', color: 'rgba(255, 255, 255, 1)',
+                                    fontWeight: '400', fontStyle: 'normal', fontSize: '1.5vw'
+                                }} ><span >03.</span>&nbsp;Skills</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-primary link" href="#contact">Contact</a>
+                                <a className="nav-link" href="#contact" style={{
+                                    position: "absolute", top: '0',
+                                    right: '19vw', width: '10vw', color: 'rgba(255, 255, 255, 1)',
+                                    fontWeight: '400', fontStyle: 'normal', fontSize: '1.5vw'
+                                }} ><span >04.</span>&nbsp;Contact</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-primary link" href="https://medium.com/@philipk179" >Articles</a>
+                                <a className="nav-link" href="https://medium.com/@philipk179" style={{
+                                    position: "absolute", top: '0',
+                                    right: '10vw', width: '10vw', color: 'rgba(255, 255, 255, 1)',
+                                    fontWeight: '400', fontStyle: 'normal', fontSize: '1.5vw'
+                                }} ><span >05.</span>&nbsp;Articles</a>
+                            </li>
+                            <li className="nav-item">
+                                <div style={{ position: 'relative', right: '2vw', width: '8vw', height: '7vh' }}>
+                                    <a className="nav-link" href="https://medium.com/@philipk179" style={{
+                                        position: "absolute", top: '0vh', border: '2px solid #5CADA9', borderRadius: '10px',
+                                        padding: '1vh 2vw', textAlign: 'center',
+                                        right: '0', color: 'rgba(255, 255, 255, 1)',
+                                        fontWeight: '400', fontStyle: 'normal', fontSize: '1.5vw'
+                                    }} >Resume</a>
+                                </div>
                             </li>
                         </ul>
                     </div>
@@ -37,3 +70,7 @@ const Nav = () => {
 export default Nav
 
 
+// position: absolute;
+//     top: 0;
+//     width: 7vw;
+//     right: 27vw;
