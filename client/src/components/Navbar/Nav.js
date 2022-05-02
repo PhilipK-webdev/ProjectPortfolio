11f7034a09f6444f05d6a2253de14069dc7537e9
Philip K <philipk179@gmail.com>
2022-05-02 09:24:02 +0300

    add:install sass,delete:bootstrap library

--- client/src/components/Navbar/Nav.js
+++ client/src/components/Navbar/Nav.js
@@ -1,66 +1,26 @@
 import React from 'react'
-import "../Navbar/Nav.css"
+import "../Navbar/Nav.scss";
 const Nav = () => {
     return (
-        <div className="row m-0">
-            <div className="container-fluid p-0" id="container">
-                <nav className="navbar navbar-expand-lg">
-                    {/* <a href="#about"
-                        className="ml-auto" id="myName"><span className='p-tag'>P</span></a> */}
-                    <button className="navbar-toggler toggler-example" type="button" data-toggle="collapse"
-                        data-target="#basic-navbar-nav"
-                        aria-controls="basic-navbar-nav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="dark-blue-text"><i
-                            className="fas fa-bars fa-1x"></i></span></button>
-                    <div className="navbar-collapse collapse" id="basic-navbar-nav" style={{ marginTop: '3vh' }}>
-                        <ul className="navbar-nav ml-auto" style={{ position: "relative" }}>
-                            <li className="nav-item " >
-                                <a className="nav-link " href="#about" style={{
-                                    position: "absolute", top: '0',
-                                    right: '44vw', width: '10vw', color: 'rgba(255, 255, 255, 1)',
-                                    fontWeight: '400', fontStyle: 'normal', fontSize: '1.5vw'
-                                }}><span >01.</span>&nbsp;About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#project" style={{
-                                    position: "absolute", top: '0',
-                                    right: '36vw', width: '10vw', color: 'rgba(255, 255, 255, 1)',
-                                    fontWeight: '400', fontStyle: 'normal', fontSize: '1.5vw'
-                                }}><span >02.</span>&nbsp;Experience</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#skill" style={{
-                                    position: "absolute", top: '0',
-                                    right: '26vw', width: '10vw', color: 'rgba(255, 255, 255, 1)',
-                                    fontWeight: '400', fontStyle: 'normal', fontSize: '1.5vw'
-                                }} ><span >03.</span>&nbsp;Skills</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#contact" style={{
-                                    position: "absolute", top: '0',
-                                    right: '19vw', width: '10vw', color: 'rgba(255, 255, 255, 1)',
-                                    fontWeight: '400', fontStyle: 'normal', fontSize: '1.5vw'
-                                }} ><span >04.</span>&nbsp;Contact</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="https://medium.com/@philipk179" style={{
-                                    position: "absolute", top: '0',
-                                    right: '10vw', width: '10vw', color: 'rgba(255, 255, 255, 1)',
-                                    fontWeight: '400', fontStyle: 'normal', fontSize: '1.5vw'
-                                }} ><span >05.</span>&nbsp;Articles</a>
-                            </li>
-                            <li className="nav-item">
-                                <div style={{ position: 'relative', right: '2vw', width: '8vw', height: '7vh' }}>
-                                    <a className="nav-link" href="https://medium.com/@philipk179" style={{
-                                        position: "absolute", top: '0vh', border: '2px solid #5CADA9', borderRadius: '10px',
-                                        padding: '1vh 2vw', textAlign: 'center',
-                                        right: '0', color: 'rgba(255, 255, 255, 1)',
-                                        fontWeight: '400', fontStyle: 'normal', fontSize: '1.5vw'
-                                    }} >Resume</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
+        <div className='nav'>
+            <div className="nav__navigation">
+                <div className='nav__navigation__logo'>
+                    <a href="#about" className="nav__navigation__letter">P</a>
+                </div>
+                <nav className="nav__navigation__nav">
+                    <ul className="nav__navigation__list">
+                        <li className="nav__navigation__item">
+                            <a href="#about" className="nav__navigation__link">
+                                <span>01.</span>About
+                            </a>
+                        </li>
+                        <li className="nav__navigation__item"><a href="#experience" className="nav__navigation__link"><span>02.</span>Experience</a></li>
+                        <li className="nav__navigation__item"><a href="#skills" className="nav__navigation__link"><span>03.</span>Skills</a>
+                        </li>
+                        <li className="nav__navigation__item"><a href="#contact" className="nav__navigation__link"><span>04.</span>Contact</a></li>
+                        <li className="nav__navigation__item"><a href="#articles" className="nav__navigation__link"><span>05.</span>Articles</a></li>
+                        <li className="nav__navigation__item"><a href="#" className="nav__navigation__link"><span>06.</span>Resume</a></li>
+                    </ul>
                 </nav>
             </div>
         </div>
